401157783ef4c930b334cdd3b71ca1a01b995cfd
pedrovbo <pedrovicboliveira@gmail.com>
2022-09-28 16:34:24 -0300

    upload microservice study project

---
+++ greeting/src/main/java/br/com/microservice/greeting/configuration/GreetingConfiguration.java
@@ -0,0 +1,32 @@
+package br.com.microservice.greeting.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-service")
+public class GreetingConfiguration {
+
+    private String greeting;
+    private String defaultValue;
+
+    public GreetingConfiguration() {
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return this.defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+}
